84ac3875e1034837e44c7409ffc52b886600af5a
Daniel Uranga <daniel.uranga@gmail.com>
2020-08-13 03:03:51 -0300

    Modify build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ARCH in x86-64-bmi2 x86-64-avx2
+for ARCH in x86-64-bmi2 x86-64-avx2 x86-64-modern
 do
     for NNARCH in halfkp_256x2-32-32 halfkp_384x2-32-32 halfkp_512x2-32-32
     do
