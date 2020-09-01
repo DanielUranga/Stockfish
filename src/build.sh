65e02bbd01ff4cfb682faa6f0b2bf7b60bc3a539
Daniel Uranga <daniel.uranga@gmail.com>
2020-09-01 15:19:29 -0300

    Add "halfkp_256x3-64-64" NN architecture

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,7 @@
 
 for ARCH in x86-64-bmi2 x86-64-avx2
 do
-    for NNARCH in halfkp_256x2-32-32 halfkp_256x3-32-32 halfkp_256x2-64-64 halfkp_384x2-32-32 halfkp_384x2-64-64 halfkp_512x2-32-32
+    for NNARCH in halfkp_256x2-32-32 halfkp_256x3-32-32 halfkp_256x3-64-64 halfkp_256x2-64-64 halfkp_384x2-32-32 halfkp_384x2-64-64 halfkp_512x2-32-32
     do
         make clean
         make -j learn ARCH=$ARCH NNARCH=$NNARCH COMP=gcc
